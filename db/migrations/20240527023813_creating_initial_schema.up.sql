2a93cdb4b066b87f820925aaaf47c3d5347998d8
snjaypee-arch <sn.jaypee@gmail.com>
2024-05-26 23:50:01 -0300

    feat: add migrations

---
+++ db/migrations/20240527023813_creating_initial_schema.up.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS "Transfer" (
+	"id" bigint GENERATED ALWAYS AS IDENTITY NOT NULL UNIQUE,
+	"account_origin_id" bigint NOT NULL,
+	"account_destination_id" bigint NOT NULL,
+	"amount" bigint NOT NULL,
+	"created_at" timestamp with time zone NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "Account" (
+	"id" bigint GENERATED ALWAYS AS IDENTITY NOT NULL UNIQUE,
+	"name" text NOT NULL,
+	"cpf" text NOT NULL,
+	"secret" bigint NOT NULL,
+	"balance" bigint NOT NULL,
+	"created_at" timestamp with time zone NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+ALTER TABLE "Transfer" ADD CONSTRAINT "Transfer_fk1" FOREIGN KEY ("account_origin_id") REFERENCES "Account"("id");
+
+ALTER TABLE "Transfer" ADD CONSTRAINT "Transfer_fk2" FOREIGN KEY ("account_destination_id") REFERENCES "Account"("id");
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.3
 require github.com/google/uuid v1.6.0
 
 require (
+	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/rs/zerolog v1.33.0 // indirect
